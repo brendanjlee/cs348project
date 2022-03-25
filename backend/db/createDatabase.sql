74d74f702fa026c2ce411bdf4a4ea4da561764e0
Brendan <brendanjlee5@gmail.com>
2022-03-25 15:13:28 -0400

    Added database creation scripts

---
+++ backend/db/createDatabase.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE if not exists cs348_project_db;
+
+-- tables
+-- company
+CREATE TABLE if not exists `company` (
+  `cid` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `field_id` INT NULL,
+  `num_openings` INT NULL,
+  PRIMARY KEY (`cid`),
+	FOREIGN KEY (`field_id`) REFERENCES field(field_id)
+) ENGINE=INNODB;
+-- job
+CREATE TABLE if not exists `job` (
+	`job_id` INT NOT NULL,
+	`job_title` VARCHAR(45) NULL,
+	`job_desc` VARCHAR (100) NULL,
+	`company_id` INT NULL,
+	`min_experience` INT NULL,
+	`skill_req` INT NULL,
+	`num_applicants` INT NULL,
+	`is_active` INT NULL,
+	`date_posted` DATE Null,
+	  PRIMARY KEY (`job_id`),
+    FOREIGN KEY (`company_id`) REFERENCES company(cid),
+    FOREIGN KEY (`skill_req`) REFERENCES skill(skill_id)
+  );
+
+-- applicant
+CREATE TABLE if not exists `applicant` (
+  `applicant_id` INT NOT NULL,
+  `name` VARCHAR(45) NULL,
+  `experience` INT NULL,
+  `skill_id` INT NULL,
+  `phone` VARCHAR(45) NULL,
+  `email` VARCHAR(45) NULL,
+  PRIMARY KEY (`applicant_id`),
+  Foreign key (`skill_id`) references skill(skill_id)
+  );
+
+-- field
+CREATE TABLE if not exists `field` (
+  `field_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL AUTO_INCREMENT
+  PRIMARY KEY (`field_id`));
+
+-- skills
+CREATE TABLE if not exists `skill` (
+  `skill_id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`skill_id`));
+
+--- relationships
---
+++ backend/db/dbPopulate.sql
@@ -0,0 +1,60 @@
+--field
+INSERT INTO `field`
+(`field_id`,
+`name`)
+VALUES
+(0, 'Other'),
+(1, 'Information Technology'),
+(2, 'Architecture and Engineering'),
+(3, 'Entertainment'),
+(4, 'Health Care'),
+(5, 'Consumer Discretionary'),
+(6, 'Consumer Staples'),
+(7, 'Finance'),
+(8, 'Telecommunication Services'),
+(9, 'Utilities'),
+(10, 'Real Estate');
+--skill
+INSERT INTO `skill`
+(`skill_id`,
+`name`)
+VALUES
+(1,'Engineering'),
+(2,'Math'),
+(3,'Programming'),
+(4,'Accounting'),
+(5,'HR'),
+(6,'Management'),
+(7,'Marketing'),
+(8,'Writing'),
+(9,'Counseling'),
+(10,'Medicine'),
+(11,'Caretaking'),
+(12,'Mechanic'),
+(13,'Eletrical'),
+(14,'Plumbing'),
+(15,'Labor'),
+(16,'Driving'),
+(17,'Law'),
+(18,'Executive');
+--company
+INSERT INTO `company`
+(`cid`,
+`name`,
+`field_id`,
+`num_openings`)
+VALUES
+(1, 'Facebook', 1, 0),
+(2, 'Apple', 1, 0),
+(3, 'Amazon', 1, 0),
+(4, 'Netflix', 3, 0),
+(5, 'Google', 1, 0),
+(6, 'Burger King', 5, 0),
+(7, 'McDonalds', 5, 0),
+(8, 'Target', 6, 0),
+(9, 'Walmart', 6, 0),
+(10, 'JPMorgan Chase & Co', 7, 0),
+(11, 'Bank of America', 7, 0),
+(12, 'Wells Fargo', 7, 0),
+(13, 'GOldman Sachs', 7, 0);
+
