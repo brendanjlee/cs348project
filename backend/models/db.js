37c3dec372d06ec0780893ab69c22e8688a34034
Brendan <brendanjlee5@gmail.com>
2022-03-25 16:00:13 -0400

    Added db connection

---
+++ backend/models/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql2');
+const dbConfig = require('../config/db.config');
+
+// connect to database
+const connection = mysql.createConnection({
+  host: dbConfig.HOST,
+  user: dbConfig.USER,
+  password: dbConfig.PASSWORD,
+  database: dbConfig.DB
+})
+
+// open connetion
+connection.connect(error => {
+  if (error) throw error;
+  console.log('Connected to database');
+});
+
+module.exports= connection;
