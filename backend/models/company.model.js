4fe53fff0d5fb7d458d6090134794108d3e85ad4
Brendan <brendanjlee5@gmail.com>
2022-03-27 17:05:31 -0400

    Update company.model.js
    Added basic queries for company

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -6,3 +6,70 @@ const Company = function(company) {
   this.field_id = company.field_id;
   this.num_openings = company.num_openings;
 };
+
+// Queries
+
+// GET - find all 
+Company.findAll = (result) => {
+  let queryString = 'select * from company';
+
+  sql.query(queryString, (err, res) => {
+    if (err) {
+      console.log('Error: ', err);
+      result(null, err);
+      return;
+    }
+
+    // print out result
+    console.log('FindAll (Company): ', res);
+    result(null, res);
+  });
+};
+
+// GET - find by company name
+Company.findByName = (companyName, result) => {
+  let queryString = `select * from company where name = \'${companyName}\'`;
+
+  sql.query(queryString, (err, res) => {
+    if (err) {
+      console.log(err);
+      //result(err, null);
+      return;
+    }
+    // more than one
+    if (res.length) {
+      console.log('FindByID (Company): ', res[0]);
+      //result(null, res[0]);
+      return;
+    }
+
+    // None found
+    result({kind: "not_found"}, null);
+  });
+}
+
+// POST - create company
+Company.create = (newCompany, result) => {
+  sql.query("insert into company set ?", newCompany, (err, res) => {
+    if (err) {
+      console.log(err);
+      result(err, null);
+      return;
+    }
+    console.log("created company: ", {id: res.insertId, ...newCompany});
+    result(null, {id: res.insertId, ...newCompany});
+  });
+};
+
+module.exports = Company;
+
+
+//test
+const deet = {
+  cid: 1,
+  name: "BigTech",
+  field_id: 7,
+  num_openings: 0
+}
+
+const bigTech = new Company(deet)
