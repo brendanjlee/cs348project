21f5f71bd132ab0c712ee1fcc4ba669a722ebc6e
Brendan <brendanjlee5@gmail.com>
2022-05-04 02:39:18 -0400

    Create joinedJob.service.js

---
+++ frontend/src/services/joinedJob.service.js
@@ -0,0 +1,35 @@
+import http from '../http-common';
+
+class JoinedJobDataService {
+  // Retrieve all jobs
+  getAll() {
+    return http.get('/joinedJob');
+  }
+
+  // Retrieve by skill (string, ex: Programming)
+  getBySkill(skill_id) {
+    return http.get(`/joinedJob/byskill/${skill_id}`);
+  }
+
+  // Retrieve by min experience (int, ex: 5)
+  getByExperience(experience) {
+    return http.get(`/joinedJob/byexperience/${Number(experience)}`);
+  }
+
+  // Retrieve by company_name (string, ex: Facebook)
+  getByCompany(company_name) {
+    return http.get(`/joinedJob/bycompany/${company_name}`);
+  }
+
+  // Retrieve by field name (string, ex: Information Technology)
+  getByField(field_name) {
+    return http.get(`/joinedJob/byfield/${field_name}`);
+  }
+
+  // Update job applicant count by job_id
+  updateApplicantCount(job_id) {
+    return http.get(`/joinedJob/${job_id}`);
+  }
+}
+
+export default new JoinedJobDataService();
